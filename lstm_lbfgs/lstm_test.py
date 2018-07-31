6535eea4d094ae3d7f61b7a3bac26e9022c87f3e
Andac Demir <ademir@ece.neu.edu>
2018-07-30 23:14:21 -0400

    l-bfgs optimization on lstm

--- lstm_lbfgs/lstm_test.py
+++ lstm_lbfgs/lstm_test.py
@@ -31,7 +31,6 @@ def set_device(model):
 def str2bool(v):
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
         return True
-
     elif v.lower() in ('no', 'false', 'f', 'n', '0'):
         return False
     else:
